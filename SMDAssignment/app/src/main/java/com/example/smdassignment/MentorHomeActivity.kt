88c36db6b3b8d2419e01bdfbb8ed331255858c38
fouad-lxix <fouadikram007@gmail.com>
2024-02-13 22:55:14 +0500

    Project Screens

---
+++ SMDAssignment/app/src/main/java/com/example/smdassignment/MentorHomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.smdassignment
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MentorHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mentor_home)
+
+    }
+}
